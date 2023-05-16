8e7cffe008fbe60247517bd517b1d811434a37cb
Eric Summer <lcianr@gmail.com>
2023-05-16 11:42:48 +0900

    added lora functions

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='df_helper',
+    version='0.0.1',
+    description='df_helper',
+    url='https://github.com/djeethub/df_helper.git',
+    packages=find_packages(),
+    install_requires=[
+      'safetensors'
+    ],
+)
