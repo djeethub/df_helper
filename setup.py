ab9b04edb66da6c1dade48934984bfd2746f0fbd
Eric Summer <lcianr@gmail.com>
2023-05-16 13:24:12 +0900

    added packages

--- setup.py
+++ setup.py
@@ -2,8 +2,9 @@ from setuptools import setup, find_packages
 
 setup(
     name='df_helper',
-    version='0.0.3',
+    version='0.0.4',
     description='df_helper',
+    packages=['lora'],
     url='https://github.com/djeethub/df_helper.git',
     packages=find_packages(),
     install_requires=[
