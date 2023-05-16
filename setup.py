475f45bc3056034d0ca3cb61414d27f98f558532
Eric Summer <lcianr@gmail.com>
2023-05-16 13:47:41 +0900

    new package name

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from setuptools import setup
 
 setup(
     name='df_helper',
-    version='0.0.6',
+    version='0.0.7',
     description='df_helper',
-    packages=['lora'],
+    packages=['df_helper'],
     url='https://github.com/djeethub/df_helper.git',
     install_requires=[
       'torch', 'safetensors'
