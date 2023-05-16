950f0afb1805338c9c0f9c84f6ce5b6d4e0708c3
Eric Summer <lcianr@gmail.com>
2023-05-16 11:46:25 +0900

    t

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='df_helper',
-    version='0.0.1',
+    version='0.0.2',
     description='df_helper',
     url='https://github.com/djeethub/df_helper.git',
     packages=find_packages(),
