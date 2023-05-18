72dc7a2158aa2d1e731bd55258a58bed947e5457
Eric Summer <lcianr@gmail.com>
2023-05-18 18:04:40 +0900

    added lora alpha support?

--- df_helper/lora.py
+++ df_helper/lora.py
@@ -3,6 +3,7 @@ LORA_PREFIX_TEXT_ENCODER = "lora_te"
 
 import torch
 from safetensors.torch import load_file
+from collections import defaultdict
 
 def clear_lora(pipe):
     if hasattr(pipe, "applied_lora"):
@@ -13,25 +14,22 @@ def clear_lora(pipe):
 def load_lora(pipe, path, alpha):
     # load LoRA weight from .safetensors
     state_dict = load_file(path, pipe.device.type)
+    updates = defaultdict(dict)
+    dtype = pipe.dtype
 
-    visited = []
-
-    # directly update weight in diffusers model
-    for key in state_dict:
+    for key, value in state_dict.items():
         # it is suggested to print out the key, it usually will be something like below
         # "lora_te_text_model_encoder_layers_0_self_attn_k_proj.lora_down.weight"
+        layer, elem = key.split('.', 1)
+        updates[layer][elem] = value
 
-        # as we have set the alpha beforehand, so just skip
-        if ".alpha" in key or key in visited:
-            continue
-
-        if "text" in key:
-            layer_infos = (
-                key.split(".")[0].split(LORA_PREFIX_TEXT_ENCODER + "_")[-1].split("_")
-            )
+    # directly update weight in diffusers model
+    for layer, elems in updates.items():
+        if "text" in layer:
+            layer_infos = layer.split(LORA_PREFIX_TEXT_ENCODER + "_")[-1].split("_")
             curr_layer = pipe.text_encoder
         else:
-            layer_infos = key.split(".")[0].split(LORA_PREFIX_UNET + "_")[-1].split("_")
+            layer_infos = layer.split(LORA_PREFIX_UNET + "_")[-1].split("_")
             curr_layer = pipe.unet
 
         # find the target layer
@@ -49,30 +47,19 @@ def load_lora(pipe, path, alpha):
                 else:
                     temp_name = layer_infos.pop(0)
 
-        pair_keys = []
-        if "lora_down" in key:
-            pair_keys.append(key.replace("lora_down", "lora_up"))
-            pair_keys.append(key)
+        # get elements for this layer
+        weight_up = elems['lora_up.weight'].to(dtype)
+        weight_down = elems['lora_down.weight'].to(dtype)
+        item_alpha = elems['alpha']
+        if item_alpha:
+            item_alpha = item_alpha.item() / weight_up.shape[1]
         else:
-            pair_keys.append(key)
-            pair_keys.append(key.replace("lora_up", "lora_down"))
+            item_alpha = 1.0
 
-        device = curr_layer.weight.data.device
-        dtype = curr_layer.weight.data.dtype
         # update weight
-        if len(state_dict[pair_keys[0]].shape) == 4:
-            weight_up = state_dict[pair_keys[0]].squeeze(3).squeeze(2)
-            weight_down = (
-                state_dict[pair_keys[1]].squeeze(3).squeeze(2)
-            )
-            curr_layer.weight.data += alpha * torch.mm(
-                weight_up, weight_down
+        if len(weight_up.shape) == 4:
+            curr_layer.weight.data += alpha * item_alpha * torch.mm(
+                weight_up.squeeze(3).squeeze(2), weight_down.squeeze(3).squeeze(2)
             ).unsqueeze(2).unsqueeze(3)
         else:
-            weight_up = state_dict[pair_keys[0]]
-            weight_down = state_dict[pair_keys[1]]
-            curr_layer.weight.data += alpha * torch.mm(weight_up, weight_down)
-
-        # update visited list
-        for item in pair_keys:
-            visited.append(item)
+            curr_layer.weight.data += alpha * item_alpha * torch.mm(weight_up, weight_down)
